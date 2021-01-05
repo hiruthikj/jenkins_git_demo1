b84354967ad9c8dfba805a0d13771584d0139945
hiruthikj <hiruthik27@gmail.com>
2021-01-05 17:55:59 +0530

    added clas

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("Hello there!!!!!");
+public class hello{
+    public static void main(String[] args) {
+        System.out.println("Hello there!!!!!");
+    }
 }
